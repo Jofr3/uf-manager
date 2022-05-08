a3e81265f5891dc24b6054630e3cb9dcc02a6fdd
Fr3D0o <jofrescari@gmail.com>
2022-05-08 18:42:48 +0200

    c12

--- src/main/java/com/example/ufmanagerf/controllers/Controller_Main.java
+++ src/main/java/com/example/ufmanagerf/controllers/Controller_Main.java
@@ -1,13 +1,20 @@
 package com.example.ufmanagerf.controllers;
 
+import com.example.ufmanagerf.services.Curs.Service_Curs;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Controller_Main {
 
+    @Autowired
+    private Service_Curs CursService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model m) {
+        m.addAttribute("cursos", CursService.getAll());
         return "index";
     }
 }
